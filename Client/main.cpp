1a9a98a0f24b8d1914322492cdb359f57287641d
AngryFender <Teabiscuitperson@gmail.com>
2025-08-18 20:24:26 +0100

    Added a client project

---
+++ Client/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <boost/asio/io_context.hpp>
+
+int main()
+{
+    std::cout << "Starting Nomi-Nomi Client" << std::endl;
+    boost::asio::io_context io_context;
+
+    return 0;
+}
---
+++ src/imanager.h
@@ -0,0 +1,8 @@
+//
+// Created by angry on 18/08/25.
+//
+
+#ifndef IMANAGER_H
+#define IMANAGER_H
+
+#endif //IMANAGER_H
