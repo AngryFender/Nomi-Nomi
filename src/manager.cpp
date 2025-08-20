d69d5c1214b0061d678a607a29c84a0fa29a9dd7
AngryFender <Teabiscuitperson@gmail.com>
2025-08-20 23:51:37 +0100

    Slowly injecting acceptor into manager

--- src/manager.cpp
+++ src/manager.cpp
@@ -12,7 +12,13 @@ bool Manager::RemoveConnection(const std::string& address)
 
 void Manager::AcceptConnection(const std::shared_ptr<IConnection>& socket)
 {
+    const std::string address_port = socket->get_socket().remote_endpoint().address().to_string() + ":" +
+        std::to_string(socket->get_socket().remote_endpoint().port());
 
+    if(!_out_connections.contains(address_port))
+    {
+        _out_connections[address_port] = socket;
+    }
 }
 
 void Manager::ClearAllConnections()
--- src/manager.h
+++ src/manager.h
@@ -1,17 +1,28 @@
 #ifndef MANAGER_H
 #define MANAGER_H
+#include "iacceptor.h"
 #include "imanager.h"
 
 class Manager: public IManager {
 
 public:
-    explicit Manager(){}
+    explicit Manager(std::unique_ptr<IAcceptor> acceptor):_acceptor(std::move(acceptor))
+    {
+        _acceptor->setHandler([this](const std::shared_ptr<IConnection>& socket)
+        {
+            this->AcceptConnection(socket);
+        });
+
+    }
     ~Manager() override = default;
     void AddConnection(const tcp::endpoint& endpoint, std::shared_ptr<IConnection> socket) override;
     bool RemoveConnection(const std::string& address) override;
     void AcceptConnection(const std::shared_ptr<IConnection>& socket) override;
     void ClearAllConnections() override;
     void ReplyMessage(const std::string& address_port, const std::vector<char>& buffer) override;
+private:
+    std::unique_ptr<IAcceptor> _acceptor;
+    std::unordered_map<std::string, std::shared_ptr<IConnection>> _out_connections;
 
 };
 
