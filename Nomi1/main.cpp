5b0ddd4db589d77926f93448d25e227aee35c818
AngryFender <Teabiscuitperson@gmail.com>
2025-09-11 23:51:21 +0100

    Deleted the unused generated files in the source folder. Updated CMakelists.txt to include directories for target to include the generated files. Lastly testing the base feature of Cap'n Proto to serialise and deserialise a struct message

--- Nomi1/main.cpp
+++ Nomi1/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include "message.capnp.h"
+#include <capnp/message.h>
+#include <capnp/serialize.h>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Testing Cap'n Proto !" << std::endl;
+
+    capnp::MallocMessageBuilder message;
+    Message::Builder builder = message.initRoot<Message>();
+
+    builder.setId(69);
+    builder.setUserid(100);
+    builder.setResourceid(36);
+
+    // serialise
+    auto words = capnp::messageToFlatArray(message);
+    kj::ArrayPtr<const capnp::word> view = words.asPtr();
+
+    // deserialise
+    capnp::FlatArrayMessageReader reader(view);
+    Message::Reader msg = reader.getRoot<Message>();
+
+    std::cout<<"id = "<< msg.getId()
+    <<" user id = "<<msg.getUserid()
+    <<" resource id = "<<msg.getResourceid();
+
     return 0;
 }
