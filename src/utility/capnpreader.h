77b6bf21e741dd4d2fc65b8d7beeedd58d64cb7b
AngryFender <Teabiscuitperson@gmail.com>
2025-09-14 23:45:34 +0100

    Adding utility file to serial/deserialise Cap'n Proto messages

---
+++ src/utility/capnpreader.h
@@ -0,0 +1,27 @@
+#ifndef CAPNPREADER_H
+#define CAPNPREADER_H
+#include <message.capnp.h>
+#include <capnp/serialize.h>
+#include <kj/common.h>
+
+bool deserialise_message(std::vector<char>& buffer)
+{
+    //convert to words
+    auto words = kj::arrayPtr(reinterpret_cast<const capnp::word*>(buffer.data()),buffer.size()/sizeof(capnp::word));
+
+    //construct a reader
+    capnp::FlatArrayMessageReader reader(words);
+    Message::Reader msg = reader.getRoot<Message>();
+
+    //todo return the deserialised message
+    return true;
+}
+
+bool serialise_message()
+{
+    //todo
+    return false;
+}
+
+
+#endif //CAPNPREADER_H
