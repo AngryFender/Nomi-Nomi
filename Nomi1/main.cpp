29691be260462096b342e794bc88047a483b47a9
AngryFender <Teabiscuitperson@gmail.com>
2026-03-27 19:51:53 +0000

    Setting up ssl files

--- Nomi1/main.cpp
+++ Nomi1/main.cpp
@@ -30,27 +30,16 @@ int main(int argc, char* argv[])
     app.add_option("-c,--config", args.config_path, "Path to the configuration file")->required(true);
     CLI11_PARSE(app, argc, argv);
 
+    auto config = toml::parse_file(args.config_path);
+    const std::string cert_path = config["server"]["cert_path"].value_or("");
+    const std::string key_path = config["server"]["key_path"].value_or("");
+    const std::string cert_node_path = config["node"]["cert_path"].value_or("");
+    const std::string key_node_path = config["node"]["key_path"].value_or("");
+
     boost::asio::io_context context_client;
     boost::asio::io_context context_node;
-
     OSSL_PROVIDER_load(nullptr, "default");
     OSSL_PROVIDER_load(nullptr, "legacy");
-
-    const char* ssl_path = std::getenv("SSL_PATH");
-    if(!ssl_path)
-    {
-        logi("SSL_PATH environment variable not set");
-        return -1;
-    }
-
-    auto config = toml::parse_file(args.config_path);
-
-    std::string_view cert_path = config["server"]["cert_path"].value_or("");
-    std::string_view key_path = config["server"]["key_path"].value_or("");
-    std::string_view cert_node_path = config["node"]["cert_path"].value_or("");
-    std::string_view key_node_path = config["node"]["key_path"].value_or("");
-
-    //Abstract this to a builder
     boost::asio::ssl::context ssl_server(boost::asio::ssl::context::tls_server);
     ssl_server.set_options(
         boost::asio::ssl::context::default_workarounds |
@@ -59,8 +48,8 @@ int main(int argc, char* argv[])
         boost::asio::ssl::context::no_tlsv1 |
         boost::asio::ssl::context::no_tlsv1_1 |
         boost::asio::ssl::context::single_dh_use);
-    // ssl_server.use_certificate_chain_file(cert_path);
-    // ssl_server.use_private_key_file(key_path, boost::asio::ssl::context::pem);
+    ssl_server.use_certificate_chain_file(cert_path);
+    ssl_server.use_private_key_file(key_path, boost::asio::ssl::context::pem);
 
     boost::asio::ssl::context ssl_node(boost::asio::ssl::context::tls_server);
     ssl_node.set_options(
@@ -70,8 +59,8 @@ int main(int argc, char* argv[])
         boost::asio::ssl::context::no_tlsv1 |
         boost::asio::ssl::context::no_tlsv1_1 |
         boost::asio::ssl::context::single_dh_use);
-    // ssl_node.use_certificate_chain_file(cert_path);
-    // ssl_node.use_private_key_file(key_path, boost::asio::ssl::context::pem);
+    ssl_node.use_certificate_chain_file(cert_path);
+    ssl_node.use_private_key_file(key_path, boost::asio::ssl::context::pem);
 
 
     auto client_acceptor = std::make_unique<SSLAcceptor>(daemon_type::client, context_client, ssl_server, SERVER1_LISTENING_PORT);
