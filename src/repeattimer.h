8a094c9794de6b5d9371fd5c407a84f54e3ff5c4
AngryFender <Teabiscuitperson@gmail.com>
2026-05-04 23:56:38 +0100

    Updated RepeatTimer's constructor into private to avoid creation on stack

--- src/repeattimer.h
+++ src/repeattimer.h
@@ -7,14 +7,19 @@
 
 class RepeatTimer: public ITimer, public std::enable_shared_from_this<RepeatTimer> {
 public:
-    RepeatTimer(boost::asio::io_context& io_context, boost::asio::chrono::seconds period): timer_(io_context), period_(period)
+    static std::shared_ptr<ITimer> create(boost::asio::io_context& io_context,const boost::asio::chrono::seconds period)
     {
-
+        return std::shared_ptr<ITimer>(new RepeatTimer(io_context, period));
     }
     void start() override;
     void cancel() override;
     void set_callback(std::function<void(const boost::system::error_code&)> callback) override;
 private:
+    RepeatTimer(boost::asio::io_context& io_context, const boost::asio::chrono::seconds period): timer_(io_context),
+                                                                                                 period_(period)
+    {
+        timer_.expires_at(std::chrono::steady_clock::now());
+    }
     boost::asio::steady_timer timer_;
     boost::asio::chrono::seconds period_;
     std::function<void(const boost::system::error_code&)> callback_;
