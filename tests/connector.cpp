27191cedad3c80eed245cab252e1f618c641dae2
AngryFender <Teabiscuitperson@gmail.com>
2026-05-23 23:50:47 +0100

    Added initialisation test for the Connector Class

--- tests/connector.cpp
+++ tests/connector.cpp
@@ -1,19 +1,30 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <functional>
-#include <boost/system/error_code.hpp> // Required for your specific signature
 #include "../src/itimer.h"
+#include "../src/connector.h"
+#include "mocks/mockconnection.h"
+#include "mocks/mocktimer.h"
 
-class MockTimer : public ITimer {
-public:
-    MOCK_METHOD(void, start, (), (override));
-    MOCK_METHOD(void, cancel, (), (override));
-    MOCK_METHOD(void, set_callback, (std::function<void(const boost::system::error_code&)> callback), (override));
-};
+TEST(ConnectorTest, initialisation_test)
+{
+    const auto mock_connection = std::make_shared<MockConnection>();
+    const auto mock_repeat_timer = std::make_shared<MockTimer>();
+    auto mock_timer = std::make_unique<MockTimer>();
+    auto mock_timer_view = mock_timer.get();
 
-TEST(ConnectorTest, timer_timeout) {
-    // Arrange: Create the mock and the class under test
+    std::function<void(const boost::system::error_code& code)> captured_timer_callback;
 
+    EXPECT_CALL(*mock_connection,set_send_callback(testing::_));
+    EXPECT_CALL(*mock_connection,set_receive_callback(testing::_));
+    EXPECT_CALL(*mock_repeat_timer,set_callback(testing::_));
+    EXPECT_CALL(*mock_timer_view,set_callback(testing::_));
+
+    EXPECT_CALL(*mock_connection, async_send(std::vector{'P','I','N','G'}));
+    EXPECT_CALL(*mock_repeat_timer, start());
+    EXPECT_CALL(*mock_timer_view, start());
+
+    Connector connector(mock_connection, std::move(mock_timer), mock_repeat_timer);
 }
 
 
