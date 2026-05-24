ae6e9f633f77ef4970177427c67afeeb1b9e3f55
AngryFender <Teabiscuitperson@gmail.com>
2026-05-24 21:31:59 +0100

    Added unit tests for pong and no pong replies

--- tests/connector.cpp
+++ tests/connector.cpp
@@ -1,7 +1,6 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <functional>
-#include "../src/itimer.h"
 #include "../src/connector.h"
 #include "mocks/mockconnection.h"
 #include "mocks/mocktimer.h"
@@ -12,8 +11,6 @@ TEST(ConnectorTest, initialisation_test)
     const auto mock_repeat_timer = std::make_shared<MockTimer>();
     auto mock_timer = std::make_unique<MockTimer>();
     auto mock_timer_view = mock_timer.get();
-
-    std::function<void(const boost::system::error_code& code)> captured_timer_callback;
 
     EXPECT_CALL(*mock_connection,set_send_callback(testing::_));
     EXPECT_CALL(*mock_connection,set_receive_callback(testing::_));
@@ -27,4 +24,46 @@ TEST(ConnectorTest, initialisation_test)
     Connector connector(mock_connection, std::move(mock_timer), mock_repeat_timer);
 }
 
+TEST(ConnectorTest, pong_test)
+{
+    const auto mock_connection = std::make_shared<MockConnection>();
+    const auto mock_repeat_timer = std::make_shared<MockTimer>();
+    auto mock_timer = std::make_unique<MockTimer>();
+    auto mock_timer_view = mock_timer.get();
+
+    std::function<void(std::unique_ptr<std::vector<char>> data)> captured_set_receive_callback;
+    EXPECT_CALL(*mock_connection,set_receive_callback(testing::_))
+        .WillOnce(testing::SaveArg<0>(&captured_set_receive_callback));
+
+    Connector connector(mock_connection, std::move(mock_timer), mock_repeat_timer);
+
+    EXPECT_CALL(*mock_repeat_timer, cancel()).Times(1);
+    EXPECT_CALL(*mock_timer_view, cancel()).Times(1);
+
+    std::vector fake_data{'P','O','N','G'};
+    auto fake_packets = std::make_unique<std::vector<char>>(std::move(fake_data));
+    captured_set_receive_callback(std::move(fake_packets));
+
+}
+TEST(ConnectorTest, no_pong_test)
+{
+    const auto mock_connection = std::make_shared<MockConnection>();
+    const auto mock_repeat_timer = std::make_shared<MockTimer>();
+    auto mock_timer = std::make_unique<MockTimer>();
+    auto mock_timer_view = mock_timer.get();
+
+    std::function<void(const boost::system::error_code& code)> captured_timer_callback;
+    EXPECT_CALL(*mock_timer_view,set_callback(testing::_))
+        .WillOnce(testing::SaveArg<0>(&captured_timer_callback));
+
+    Connector connector(mock_connection, std::move(mock_timer), mock_repeat_timer);
+
+    EXPECT_CALL(*mock_timer_view, cancel()).Times(0);
+    EXPECT_CALL(*mock_repeat_timer, cancel()).Times(1); // since the 30 seconds timeout is over, the repeated timer is cancelled
+
+    boost::system::error_code code;
+    captured_timer_callback(code);
+
+
+}
 
